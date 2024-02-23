1081b99cbcb4f63ad6b48e142393a78f5f996090
MaciejMi <maciejmich22@gmail.com>
2024-02-23 16:14:26 +0100

    add 404 page

--- app.js
+++ app.js
@@ -1,38 +1,42 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const bodyParser = require('body-parser');
+const express = require('express')
+const path = require('path')
+const fs = require('fs')
+const bodyParser = require('body-parser')
 
-const PORT_NUMBER = 3000;
+const PORT_NUMBER = 3000
 
-const app = express();
+const app = express()
 
-app.set('view engine', 'pug');
+app.set('view engine', 'pug')
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/newnote', (req, res, next) => {
-	res.render('newnote');
-});
+	res.render('newnote', { title: 'My notes - new note' })
+})
 
 app.get('/', (req, res, next) => {
-	const data = require('./data/notes.json');
-	res.render('index', { notes: data });
-});
+	const data = require('./data/notes.json')
+	res.render('index', { title: 'My notes - main', notes: data })
+})
 
 app.post('/newnote', (req, res, next) => {
-	const title = req.body.title;
-	const message = req.body.message;
+	const title = req.body.title
+	const message = req.body.message
 
-	const data = require('./data/notes.json');
-	const newNote = { title: title, message: message };
+	const data = require('./data/notes.json')
+	const newNote = { title: title, message: message }
 
-	data.push(newNote);
+	data.push(newNote)
 
 	fs.writeFile('./data/notes.json', JSON.stringify(data), () => {
-		res.redirect('/');
-	});
-});
+		res.redirect('/')
+	})
+})
 
-app.listen(PORT_NUMBER);
+app.use('/', (req, res, next) => {
+	res.render('404', { title: '404' })
+})
+
+app.listen(PORT_NUMBER)
